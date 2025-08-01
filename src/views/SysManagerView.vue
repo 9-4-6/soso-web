d469d0bc874c874cb246ce14f36df481bf2c25c7
guozhong <17853522814@163.com>
2025-08-01 17:11:13 +0800

    系统管理

--- src/views/SysManagerView.vue
+++ src/views/SysManagerView.vue
@@ -1,70 +1,40 @@
 <script lang="ts" setup>
-import {
-    DataBoard,
-    Document,
-} from '@element-plus/icons-vue'
 
-const isCollapse = ref(true)
-const handleOpen = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-}
 </script>
 <template>
     <div class="app-container">
-        <div class="header">
+        <div class="nav">顶部区域</div>
+        <div class="main-content">
+            <div class="left-side">左侧边栏</div>
+            <div class="content">主内容区</div>
         </div>
-        <div class="menu">
-            <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse" @open="handleOpen"
-                @close="handleClose">
-                <el-sub-menu index="1">
-                    <template #title>
-                        <el-icon>
-                            <Document />
-                        </el-icon>
-                        <span>系统管理</span>
-                    </template>
-                    <el-menu-item index="1-1">
-                        <template #title>部门管理</template>
-                    </el-menu-item>
-                    <el-menu-item index="1-2">
-                        <template #title>岗位管理</template>
-                    </el-menu-item>
-                    <el-menu-item index="1-3">
-                        <template #title>角色管理</template>
-                    </el-menu-item>
-                    <el-menu-item index="1-4">
-                        <template #title>人员管理</template>
-                    </el-menu-item>
-                </el-sub-menu>
-                <el-menu-item index="2">
-                    <el-icon>
-                        <DataBoard />
-                    </el-icon>
-                    <template #title>运营管理</template>
-                </el-menu-item>
-            </el-menu>
-        </div>
-
     </div>
 
 </template>
-
-
-
 <style lang="less" scoped>
 .app-container {
+    display: flex;
+    flex-direction: column;
     height: 100vh;
 
+    .nav {
+        flex: 0 0 10%;
+        background: #f0f0f0;
+    }
 
-    .menu {
-        margin-top: 50px;
+    .main-content {
+        flex: 0 0 90%;
+        display: flex;
 
-        .el-menu-vertical-demo:not(.el-menu--collapse) {
-            width: 200px;
-            min-height: 400px;
+        .left-side {
+            flex: 0 0 10%;
+            background: #e6f7ff;
+            border-right: 1px solid #d9d9d9;
+        }
+
+        .content {
+            flex: 0 0 90%;
+            background: #fff;
         }
     }
 }
